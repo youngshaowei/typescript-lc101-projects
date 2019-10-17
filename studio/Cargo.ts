6447600dc63a6ea66e093d3a2fcc3d84aa33e1cd
Krystal Farwig <youngshaowei@gmail.com>
2019-10-17 18:58:21 -0500

    completed cargo

--- studio/Cargo.ts
+++ studio/Cargo.ts
@@ -2,4 +2,10 @@ import {Payload} from './Payload';
 
 export class Cargo implements Payload {
     // properties and methods
+    massKg: number;
+    material: string;
+    constructor(massKg: number, material: string) {
+        this.massKg = massKg;
+        this.material = material;
+    }
  }
