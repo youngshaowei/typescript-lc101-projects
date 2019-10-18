8994345db5d7e316fa1a37bfbc4b79be2479a116
Krystal Farwig <youngshaowei@gmail.com>
2019-10-17 19:26:31 -0500

    cried from confusion

--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -1,5 +1,20 @@
 import {Payload} from './Payload';
+import {Astronaut} from './Astronaut';
+import {Cargo} from './Cargo';
+
 
 export class Rocket implements Payload {
     // properties and methods
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[];
+    astronauts: Astronaut[];
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+    sumMass(items: Payload[]): number {
+        for (let i=0; i<Payload.length; i++)
+        
+    }
  }
