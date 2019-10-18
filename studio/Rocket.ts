083f610460e934312cf8ff19fbba2c4990cbf569
Krystal Farwig <youngshaowei@gmail.com>
2019-10-17 19:59:19 -0500

    completed rocket and copy/pasted into Index

--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -9,12 +9,41 @@ export class Rocket implements Payload {
     totalCapacityKg: number;
     cargoItems: Cargo[];
     astronauts: Astronaut[];
+    massKg: number;
     constructor(name: string, totalCapacityKg: number) {
         this.name = name;
         this.totalCapacityKg = totalCapacityKg;
     }
+
     sumMass(items: Payload[]): number {
-        for (let i=0; i<Payload.length; i++)
-        
+        let sum: number = 0;
+        for (let i=0; i<items.length; i++) {
+            sum =+ items[i].massKg
+        }
+        return sum;
+    }
+    currentMassKg(): number {
+        return this.sumMass(this.astronauts)+this.sumMass(this.cargoItems);
+    }
+    canAdd(item: Payload): boolean {
+        if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
+            return true;
+        }
+    }
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo)){
+            this.cargoItems.push(cargo);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut)){
+            this.astronauts.push(astronaut);
+            return true;
+        } else {
+            return false;
+        }
     }
  }
